0a571b31b012dc13f7f51147bd794bd226ee6a33
Uday Daroch <uda12@uclive.ac.nz>
2026-03-29 20:02:24 +1300

    no github link its private

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -43,7 +43,6 @@ export const projects: Project[] = [
       "JWT",
     ],
     link: "http://k8s-prism-prisming-8596357315-1014118624.ap-southeast-2.elb.amazonaws.com",
-    github: "https://github.com/udaydaroch/mini-prism",
     image:mimiPrism
   },
   {
